4cced0f30957372e5f5e9c1eb9109b297bb105ba
Artem Rubchenko <65110011+RubchenkoArtem@users.noreply.github.com>
2024-01-05 11:46:02 +0300

    1
    1

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -1,18 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private bool dogSpawnReady;
+
+    private void Start()
+    {
+        dogSpawnReady = false;
+    }
+
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
-        }
+        if (Input.GetKeyDown(KeyCode.Space) && dogSpawnReady == false)
+        StartCoroutine(makeDogs());
     }
+
+    IEnumerator makeDogs()
+    {
+        dogSpawnReady = true;
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        yield return new WaitForSeconds(1);
+        dogSpawnReady = false;
+    }
+
 }
